a63c1de35f3d3c979dd7878ba95619e61046ac19
akhadeli <akhadeli@ualberta.ca>
2025-01-28 23:52:06 -0700

    updated

--- src/app/exercises/ex1/page.tsx
+++ src/app/exercises/ex1/page.tsx
@@ -11,6 +11,17 @@ export default function Home() {
         />
         <div className="flex flex-col gap-0.5">
           <h1 className="text-4xl font-bold">CMPUT 412/503 - Exercise 1</h1>
+          <p className="text-sm text-gray-500">
+            In this first exercise you will learn the basic knowledge and skills
+            you need to operate a Duckiebot. This exercise will be marked
+            individually, although we encourage you to work together with your
+            labmates. It’s to your benefit to read the whole lab, and keep an
+            eye on the marking guide as you progress through the steps. Now,
+            let’s get quacking! TIP: Read carefully. Most of the issues I
+            encountered myself, and things I had a hard time finding, were
+            because I was trying to rush through and skimming for commands to
+            run.
+          </p>
         </div>
       </div>
       <hr className="my-4 mt-8" />
@@ -19,8 +30,11 @@ export default function Home() {
         href="https://github.com/akhadeli/cmput412-akhadeli-code/blob/main/exercise-1/report/CMPUT%20412%20-%20Exercise%201%20Report.pdf"
         className="text-blue-500 hover:text-blue-700"
       >
-        Report
+        Click here to view the report
       </Link>
+      <p className="mt-4">
+        The report is a PDF file that contains the report for Exercise 1.
+      </p>
     </main>
   );
 }
